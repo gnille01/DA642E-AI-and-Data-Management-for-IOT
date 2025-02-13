7c564079f6fa544449e1d524f49bab3d56179fcb
gnille01 <53049451+gnille01@users.noreply.github.com>
2025-02-13 19:32:34 +0100

    Edit

--- AIDM_Lab2-A2.2/plot_data.py
+++ AIDM_Lab2-A2.2/plot_data.py
@@ -1,10 +1,12 @@
 """
-II- Visualize the data with a line graph with two axes: time & temperature with criteria below (1pt-Mandatory)
+I- Collect the data in a CSV file and submit it with the rest of your results (2pts-Mandatory)
 
-    The color of the line should be orange
-    Add labels for each axes (Temperature (degrees Celsius), Time(seconds)),
+II- Visualize the data with a line graph with two axes with criteria below (1pt-Mandatory)
+
+    plot 6 axis of data
+    Add labels for each axes (Acceleration (m.sq/s.sq), Time(seconds)),
     Turn on the grids 
-    Add legend on the top right corner - temperature
+    Add legend on the top right corner - the name of your plots should be Ax, Ay, Az, Gx, Gy, Gz
 
 """
 
@@ -17,12 +19,19 @@ data = pd.read_csv(file_path, delimiter=',')
 print(data)
 
 fig, ax = plt.subplots()
-ax.plot(data['Timesteps'], data[' Temperature'], color='orange')
+ax.plot(data[' Ax'], data[' Ay'], data[' Az'])
 ax.set_xlabel('Time (seconds)')
-ax.set_ylabel('Temperature (degrees Celsius)')
+ax.set_ylabel('Accelerometer')
 ax.grid(True)
-ax.legend(['Temperature'], loc='upper right')
+ax.legend(['Accelerometer logging'])
 
-plt.savefig('data/plot_temp.png')
+"""ax.plot(data[' Gx'], data[' Gy'], data[' Gz'])
+ax.set_xlabel('Time (seconds)')
+ax.set_ylabel('Gyroscope')
+ax.grid(True)
+ax.legend(['Gyroscope logging'])"""
+
+
+plt.savefig('data/plot_Acceleration.png')
 plt.show()
 plt.close()
