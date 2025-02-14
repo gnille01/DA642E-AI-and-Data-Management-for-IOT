ef707ac68ea0d81f6a322db61c511280973bfedf
gnille01 <53049451+gnille01@users.noreply.github.com>
2025-02-14 15:44:36 +0100

    Task_II

---
+++ LAB_2/AIDM_Lab2-A2.4/Task_I.py
@@ -0,0 +1,59 @@
+"""
+I- 
+    Based on the instruction on the distribution transformation, 
+    transform the "calories" column to take the shape of a distribution close to normal distribution. 
+    The current distribution looks something like the below figure. 
+    Experiment with different transforms (log, cube, etc.) to find the right one. 
+    Use a transform to make the data distribution more consistent, 
+    meaning there are values on each column (1pts - Mandatory)
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+file_path = 'LAB_2/AIDM_Lab2-A2.4/aw_fb_data.csv'
+
+data = pd.read_csv(file_path, delimiter=',') 
+
+print(data['calories']) 
+
+# copy the data
+transform = data.copy()
+
+# subplots
+fig, ax = plt.subplots(2, 2, figsize=(12, 10))
+
+# Original histogram
+ax[0, 0].hist(transform['calories'])
+ax[0, 0].set_title('Calories frequency distribution')
+ax[0, 0].set_xlabel('Calories')
+ax[0, 0].set_ylabel('Frequency')
+#ax[0, 0].set_yscale('log')
+
+# Log1p transform
+transform['log'] = transform['calories'].transform(np.log10)
+ax[0, 1].hist(transform['log'])#, bins=30, density=True
+ax[0, 1].set_title('Log10 of Calories Distribution')
+ax[0, 1].set_xlabel('Log10Calories')
+ax[0, 1].set_ylabel('Frequency')
+#ax[0, 1].set_yscale('log')
+
+# Cube transform, for cube root = np.power(x, 1/3)
+transform['cube'] = transform['calories'].transform(lambda x: np.power(x, 3))
+ax[1, 0].hist(transform['cube']) #, bins=30, density=True
+ax[1, 0].set_title('Cube of Calories Distribution')
+ax[1, 0].set_xlabel('Cube Calories')
+ax[1, 0].set_ylabel('Frequency')
+
+
+#Cube root transform
+transform['cube_root'] = transform['calories'].transform(lambda x: np.power(x, 1/3))
+ax[1, 1].hist(transform['cube_root']) #, bins=30, density=True
+ax[1, 1].set_title('Cube root of Calories Distribution')
+ax[1, 1].set_xlabel('Calories')
+ax[1, 1].set_ylabel('Frequency')
+
+# Save &/or display
+plt.savefig('LAB_2/AIDM_Lab2-A2.4/Task-I_calories_transformations.png')
+##plt.show()
---
+++ LAB_2/AIDM_Lab2-A2.4/Task_II.py
@@ -0,0 +1,67 @@
+"""
+II- 
+    As mentioned before, the data reflects 49 participants. 
+    Make a copy of the original dataframe and Find a way to keep one sample from each participant.  
+    Therefore, the new dataframe should have 49 rows. 
+    You should use a specific function or a mix of functions in the instruction. 
+    Afterward, visualize the "age", "height", 
+    and "weight" of the participants on each subplot (stacked plot). 
+    Grids should be on, Legends should be on top, 
+    and The color of the line plot for each subplot should be different. (2pts - Optional)
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+file_path = 'LAB_2/AIDM_Lab2-A2.4/aw_fb_data.csv'
+
+data = pd.read_csv(file_path, delimiter=',')
+
+# copy the data
+df = data.copy()
+
+# drop duplicates of the same participant based on age, gender, height
+df.drop_duplicates(subset=('age','gender','height'), keep='first', inplace=True)
+df.reset_index(drop=True, inplace=True)
+print("Row, Col: ", df.shape)
+print(df.head())
+print(df.tail())
+
+df.to_csv('LAB_2/AIDM_Lab2-A2.4/Task-II_aw_fb_data.csv', index=False)
+
+# Create subplots: 3 rows, 1 column
+fig, ax = plt.subplots(2, 2, figsize=(10, 12))
+
+# Stackplot for Age
+ax[0, 0].stackplot(df.index, df['age'], labels=['Age'], colors=['blue'])
+ax[0, 0].set_title('Age of participants')
+ax[0, 0].set_ylabel('Age')
+ax[0, 0].set_xlabel('Participant')
+ax[0, 0].legend()
+
+# Stackplot for Height
+ax[1, 0].stackplot(df.index, df['height'], labels=['Height'], colors=['green'])
+ax[1, 0].set_title('Height of participants')
+ax[1, 0].set_ylabel('Height ')
+ax[1, 0].set_xlabel('Participant')
+ax[1, 0].legend()
+
+# Stackplot for Weight
+ax[0, 1].stackplot(df.index, df['weight'], labels=['Weight'], colors=['red'])
+ax[0, 1].set_title('Weight of participants')
+ax[0, 1].set_ylabel('Weight ')
+ax[0, 1].set_xlabel('Participant')
+ax[0, 1].legend()
+
+# Stackplot for Age, Height, Weight
+ax[1, 1].stackplot(df.index, df['age'], df['height'], df['weight'], labels=['Age', 'Height', 'Weight'])
+ax[1, 1].set_title('Age, Height, Weight of participants')
+ax[1, 1].set_ylabel('Age, Height, Weight')
+ax[1, 1].set_xlabel('Participant')
+ax[1, 1].legend()
+
+# Adjust layout & Show
+plt.tight_layout()
+plt.savefig('LAB_2/AIDM_Lab2-A2.4/Task-II_age_height_weight.png')
+plt.show()
+
