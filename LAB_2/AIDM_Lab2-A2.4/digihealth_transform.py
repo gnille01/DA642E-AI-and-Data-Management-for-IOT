92f2bc26fcfde65e0bafc981897303876d86797a
gnille01 <53049451+gnille01@users.noreply.github.com>
2025-02-14 10:04:00 +0100

    Task A2.4 - Restructuring repo

--- LAB_2/AIDM_Lab2-A2.4/digihealth_transform.py
+++ LAB_2/AIDM_Lab2-A2.4/digihealth_transform.py
@@ -12,7 +12,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-file_path = 'AIDM_Lab2-A2.4/aw_fb_data.csv'
+file_path = 'LAB_2/AIDM_Lab2-A2.4/aw_fb_data.csv'
 
 data = pd.read_csv(file_path, delimiter=',') 
 
@@ -23,7 +23,7 @@ data['calories'].hist() #The function plots the distribution of the data in the 
 plt.xlabel('Calories')
 plt.ylabel('Frequency')
 plt.title('Calories frequency distribution')
-plt.savefig('AIDM_Lab2-A2.4/calories_histogram.png') # Save the histogram 
+plt.savefig('LAB_2/AIDM_Lab2-A2.4/calories_histogram.png') # Save the histogram 
 ## plt.show() # Display the plot
 
 # transfrom the data
@@ -35,20 +35,20 @@ transform['log'].hist()
 plt.title('Log10 of Calories Distribution')
 plt.xlabel('Calories')
 plt.ylabel('Frequency')
-plt.savefig('AIDM_Lab2-A2.4/calories_log_histogram.png') # Save the log plot
+plt.savefig('LAB_2/AIDM_Lab2-A2.4/calories_log_histogram.png') # Save the log plot
 ## plt.show() # Display the plot
 
 # Cube transform
 """transform['cube'] = transform['calories'].transform(lambda x: np.power(x, 3))
 transform['cube'].hist()"""
 
-# Cube root transform, github copilot suggestions
+# Cube root transform, github copilot suggestion
 transform['cube_root'] = transform['calories'].transform(lambda x: np.power(x, 1/3))
 transform['cube_root'].hist()
 
 plt.xlabel('Calories')
 plt.ylabel('Frequency')
 plt.title('Cube Root of Calories Distribution')
-plt.savefig('AIDM_Lab2-A2.4/calories_cube_histogram.png') # Save the cube plot
+plt.savefig('LAB_2/AIDM_Lab2-A2.4/calories_cube_histogram.png') # Save the cube plot
 ## plt.show() # Display the plot
 
